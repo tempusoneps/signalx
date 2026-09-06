0d40fb8e077a9f3a3be507a4f66575410af669a5
Zuongthao <zuongthao.tn@gmail.com>
2026-09-07 06:25:51 +0700

    develop(v0.1.0): integrate smc signals into pipeline orchestration and composite confluence

--- src/signalx/signals/__init__.py
+++ src/signalx/signals/__init__.py
@@ -8,6 +8,7 @@ from signalx.metadata import to_code_names
 from signalx.signals.candlestick import generate_candlestick_signals
 from signalx.signals.composite import generate_composite_signals
 from signalx.signals.momentum import generate_momentum_signals
+from signalx.signals.smc import SMC_SIGNAL_COLUMNS, generate_smc_signals
 from signalx.signals.statistical import generate_statistical_signals
 from signalx.signals.trend import generate_trend_signals
 from signalx.signals.volatility import generate_volatility_signals
@@ -42,9 +43,10 @@ def run_all_signal_generators(
     vol_df = generate_volatility_signals(normalized, show_progress=show_progress)
     volume_df = generate_volume_signals(normalized, show_progress=show_progress)
     cdl_df = generate_candlestick_signals(normalized, show_progress=show_progress)
+    smc_df = generate_smc_signals(normalized, show_progress=show_progress)
     stat_df = generate_statistical_signals(normalized, show_progress=show_progress)
 
-    intermediate = pd.concat([trend_df, mom_df, vol_df, volume_df, cdl_df, stat_df], axis=1)
+    intermediate = pd.concat([trend_df, mom_df, vol_df, volume_df, cdl_df, smc_df, stat_df], axis=1)
     comp_df = generate_composite_signals(normalized, intermediate, show_progress=show_progress)
 
     full_signals = pd.concat([intermediate, comp_df], axis=1)
@@ -55,9 +57,11 @@ def run_all_signal_generators(
 
 
 __all__ = [
+    "SMC_SIGNAL_COLUMNS",
     "generate_candlestick_signals",
     "generate_composite_signals",
     "generate_momentum_signals",
+    "generate_smc_signals",
     "generate_statistical_signals",
     "generate_trend_signals",
     "generate_volatility_signals",
